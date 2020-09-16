06136724bc70ca9da2b357e2fb504e3e2abef0f9
Yearn Finance Passive Income <70851427+yfpi@users.noreply.github.com>
2020-09-16 16:00:42 +0530

    contract completed

---
+++ contracts/yfpi.sol
@@ -0,0 +1,345 @@
+pragma solidity ^0.5.13;
+
+// ----------------------------------------------------------------------------
+// Safe maths
+// ----------------------------------------------------------------------------*/
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public view returns (uint256);
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        returns (uint256 balance);
+
+    function allowance(address tokenOwner, address spender)
+        public
+        view
+        returns (uint256 remaining);
+
+    function transfer(address to, uint256 tokens) public returns (bool success);
+
+    function approve(address spender, uint256 tokens)
+        public
+        returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
+}
+
+// ----------------------------------------------------------------------------
+// Contract function to receive approval and execute function in one call
+//
+// Borrowed from MiniMeToken
+// ----------------------------------------------------------------------------
+contract ApproveAndCallFallBack {
+    function receiveApproval(
+        address from,
+        uint256 tokens,
+        address token,
+        bytes memory data
+    ) public;
+}
+
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+    address payable public owner;
+    address payable public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address payable _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+
+// ----------------------------------------------------------------------------
+// ERC20 Token, with the addition of symbol, name and decimals and a
+// MYT supply
+// ----------------------------------------------------------------------------
+contract MyToken is ERC20Interface, Owned {
+    using SafeMath for uint256;
+
+    string public symbol;
+    string public name;
+    uint8 public decimals;
+    uint256 _totalSupply;
+    bool public isRoundOneActive;
+    bool public isRoundTwoActive;
+    uint256 public PresaleToken;
+    uint256 private TokenLimit;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+    constructor() public {
+        symbol = "MYT";
+        name = "MyToken Token";
+        decimals = 18;
+        _totalSupply = 30000 * 10**uint256(decimals);
+        PresaleToken = 15000 * 10**uint256(decimals);
+        TokenLimit = 6000 * 10**uint256(decimals);
+        balances[owner] = _totalSupply - PresaleToken;
+        isRoundOneActive = true;
+        isRoundTwoActive = false;
+        emit Transfer(address(0), owner, _totalSupply - PresaleToken);
+    }
+
+    // ------------------------------------------------------------------------
+    // Total supply
+    // ------------------------------------------------------------------------
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply.sub(balances[address(0)]);
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Get the token balance for account `tokenOwner`
+    // ------------------------------------------------------------------------
+    function balanceOf(address tokenOwner)
+        public
+        view
+        returns (uint256 balance)
+    {
+        return balances[tokenOwner];
+    }
+
+    // ------------------------------------------------------------------------
+    // Transfer the balance from token owner's account to `to` account
+    // - Owner's account must have sufficient balance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transfer(address to, uint256 tokens)
+        public
+        returns (bool success)
+    {
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for `spender` to transferFrom(...) `tokens`
+    // from the token owner's account
+    //
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+    // recommends that there are no checks for the approval double-spend attack
+    // as this should be implemented in user interfaces
+    // ------------------------------------------------------------------------
+    function approve(address spender, uint256 tokens)
+        public
+        returns (bool success)
+    {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Transfer `tokens` from the `from` account to the `to` account
+    //
+    // The calling account must already have sufficient tokens approve(...)-d
+    // for spending from the `from` account and
+    // - From account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public returns (bool success) {
+        balances[from] = balances[from].sub(tokens);
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
+    function allowance(address tokenOwner, address spender)
+        public
+        view
+        returns (uint256 remaining)
+    {
+        return allowed[tokenOwner][spender];
+    }
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for `spender` to transferFrom(...) `tokens`
+    // from the token owner's account. The `spender` contract function
+    // `receiveApproval(...)` is then executed
+    // ------------------------------------------------------------------------
+    function approveAndCall(
+        address spender,
+        uint256 tokens,
+        bytes memory data
+    ) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(
+            msg.sender,
+            tokens,
+            address(this),
+            data
+        );
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    function() external payable {
+      
+        uint256 tokens;
+         if(isRoundOneActive == true){
+           tokens = msg.value * 36;
+           require(tokens  <= (PresaleToken - TokenLimit));
+        } else if(isRoundTwoActive ==true) {
+            tokens = msg.value * 24;
+            require(tokens <= PresaleToken);
+        }
+        else {
+            require(isRoundOneActive == true || isRoundOneActive == true);
+        }
+        
+        balances[msg.sender] += tokens;
+        PresaleToken -= tokens;
+        // sent to investor
+        emit Transfer(address(this), msg.sender, tokens);
+        // sent ETH to owner
+        owner.transfer(msg.value);
+    }
+
+    
+  function buyToken()
+        public payable
+        {
+            uint256 tokens;
+         if(isRoundOneActive == true){
+           tokens = msg.value * 36;
+           require(tokens  <= (PresaleToken - TokenLimit));
+        } else if(isRoundTwoActive ==true) {
+            tokens = msg.value * 24;
+            require(tokens <= PresaleToken);
+        }
+        else {
+            require(isRoundOneActive == true || isRoundOneActive == true);
+        }
+        require(tokens <= PresaleToken);
+        balances[msg.sender] += tokens;
+        PresaleToken -= tokens;
+        // sent to investor
+        emit Transfer(address(this), msg.sender, tokens);
+        // sent ETH to owner
+        owner.transfer(msg.value);
+        }
+
+    // ------------------------------------------------------------------------
+    // Owner can transfer out any accidentally sent ERC20 tokens
+    // ------------------------------------------------------------------------
+    function transferAnyERC20Token(address tokenAddress, uint256 tokens)
+        public
+        onlyOwner
+        returns (bool success)
+    {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
+
+    function activateRound(int8 round)
+        public onlyOwner
+        returns (bool success)
+    {
+        if(round==1){
+            isRoundOneActive = true;
+            isRoundTwoActive = false;
+            return true;
+        } else if(round==2) {
+            isRoundOneActive = false;
+            isRoundTwoActive = true;
+            return true;
+        }
+        else if(round==0){
+            isRoundOneActive = false;
+            isRoundTwoActive = false;
+            return true;
+        }
+        return true;
+    }
+
+     function closePresale()
+        public onlyOwner
+        returns (bool success)
+    {
+        balances[msg.sender] += PresaleToken;
+        PresaleToken -= PresaleToken;
+        emit Transfer(address(this),msg.sender, PresaleToken);
+        return true;
+    }
+
+     function burn(uint256 amount) public returns (bool success){
+        require(msg.sender != address(0), "ERC20: burn from the zero address");
+        balances[msg.sender] = balances[msg.sender].sub(amount);
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(msg.sender, address(0), amount);
+         return true;
+    }
+}
---
+++ test/Inbox.test.js
@@ -0,0 +1,109 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+const contractP = require("../compile");
+const contract = JSON.parse(contractP).contracts["yfpi.sol"]["MyToken"];
+const abi = contract.abi;
+const bytecode = contract.evm.bytecode.object;
+
+let accounts;
+let inbox;
+let ac0balance;
+beforeEach(async() => {
+    //get list of all accounts
+    // web3.eth.getAccounts().then((fechtedAccounts) => { console.log(fechtedAccounts); });
+
+    accounts = await web3.eth.getAccounts();
+    ac0balance = await web3.eth.getBalance(accounts[0]);
+
+    //use all of those accounts to deploy contract
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: "2000000" });
+
+});
+
+describe("Inbox", () => {
+    it("show account", () => {
+        // console.log(accounts);
+    });
+    it("Show Contract", () => {
+        // console.log(inbox);
+    });
+    it("deploys a contract", () => {
+        assert.ok(inbox.options.address);
+    });
+    it("has total supply", async() => {
+        const message = await inbox.methods.totalSupply().call();
+        // console.log(message);
+        assert.equal(message, "30000000000000000000000");
+    });
+    it("has balance", async() => {
+        const message = await inbox.methods.balanceOf(accounts[0]).call({ from: accounts[0] });
+        console.log(message);
+        assert.equal(message, "15000000000000000000000");
+    });
+    it("has balance in second account", async() => {
+        const message = await inbox.methods.balanceOf(accounts[1]).call({ from: accounts[0] });
+        console.log(message);
+        // assert.equal(message, "30000000000000000000000");
+    });
+    it("transfers balance in second account", async() => {
+        const message1 = await inbox.methods.transfer(accounts[1], "1000000000000000000000").send({ from: accounts[0] });
+        console.log(message1);
+        const message2 = await inbox.methods.balanceOf(accounts[1]).call({ from: accounts[0] });
+        console.log(message2);
+        // assert.equal(message, "30000000000000000000000");
+    });
+    it("account 0 balance", async() => {
+        console.log(
+            await web3.eth.getBalance(accounts[0])
+        );
+    });
+    it('allows one account to enter', async() => {
+
+        console.log(
+            await web3.eth.getBalance(accounts[0])
+        );
+    });
+
+    it("isRoundOneActive", async() => {
+        const message = await inbox.methods.isRoundOneActive().call();
+        console.log(message);
+        // assert.equal(message, "30000000000000000000000");
+    });
+    it("isRoundTwoActive", async() => {
+        const message = await inbox.methods.isRoundTwoActive().call();
+        console.log(message);
+        // assert.equal(message, "30000000000000000000000");
+    });
+
+    it("activate round one", async() => {
+        const message = await inbox.methods.activateRound(1).send({ from: accounts[0] });
+        console.log(message);
+        const message1 = await inbox.methods.isRoundOneActive().call();
+        console.log(message1);
+        // assert.equal(message, "30000000000000000000000");
+    });
+    it("activate round two", async() => {
+        const message = await inbox.methods.activateRound(2).send({ from: accounts[0] });
+        // console.log(message);
+        const message1 = await inbox.methods.isRoundTwoActive().call();
+        console.log(message1);
+        // assert.equal(message, "30000000000000000000000");
+    });
+    it("activate round two and deactivate", async() => {
+        const message = await inbox.methods.activateRound(2).send({ from: accounts[0] });
+        // console.log(message);
+        const message1 = await inbox.methods.isRoundTwoActive().call();
+        console.log(message1);
+        const message2 = await inbox.methods.activateRound(0).send({ from: accounts[0] });
+        //  console.log(message2);
+        const message3 = await inbox.methods.isRoundTwoActive().call();
+        console.log(message3);
+        // assert.equal(message, "30000000000000000000000");
+    });
+
+});
